367c9588da5a9aa0d705f8ed0adb7aeff5d6662b
Nick Matantsev <nick@sigmaxis.com>
2011-05-21 02:30:50 -0400

    Misc cleanup.

--- test/store/StoreTest.java
+++ test/store/StoreTest.java
@@ -6,10 +6,6 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 import static org.mockito.Mockito.*;
 
-/**
- *
- * @author Nikita
- */
 public class StoreTest {
     Store.Backend backend;
 
